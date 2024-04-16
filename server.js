87b82e9694c234207cea32240f4ba788f3212396
Justnme <tpopokghfjpoik@gmail.com>
2024-04-16 21:06:00 +0300

    #148 Adjusted path problem

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
-const express = require('express');
-const hbs = require("hbs");
+
 // for mysql
 // const mysql = require('mysql');
 
-
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+const express = require('express');
+const hbs = require("hbs");
 const app = express();
 const port = 3000;
 
-//const bodyParser = require('body-parser');
-//app.use(bodyParser.json());
-app.use(express.json());
+app.set("view engine", "hbs"); // Install hbs
+app.set("views", __dirname + "/views"); // Set views directory
 
-app.use(express.static(__dirname)); //Set the default directory path to "book-store-main"
+app.use(express.static(__dirname)); // Set the default directory path to serve static files
 
-app.set("view engine", "hbs"); //Install hbs
-
-app.get("/", function (_, response) { //Making it so localhost:3000 is turned into localhost:3000/home
-    response.sendFile(__dirname + "/index.hbs");
+app.get("/", (_, response) => {
+    response.render("index"); // Render views/index.hbs
 });
 
-app.use("/home", function(request, response){ //show hbs files
-    response.render("index.hbs");
+app.get("/home", (_, response) => {
+    response.render("index"); // Render views/index.hbs
 });
 
-app.use("/genres", function(request, response){
-    response.render("genres.hbs");
-});
-
-app.get('/', (req, res) => {
-	console.log('Listening!');
+app.get("/genres", (_, response) => {
+    response.render("genres"); // Render views/genres.hbs
 });
 
 app.listen(port, () => {
-	console.log(`Server is listening on localhost:${port}`);
+    console.log(`Server is listening on localhost:${port}`);
 });
+
 
 // const connection = mysql.createConnection({
 //     host: 'localhost',
