ce4fa6fed41210398e1c528e6d645bb8b834f92e
NikitaGuy <k21_zykov.my@server.odessa.ua>
2024-04-16 12:02:10 +0300

    Made site work with localhost and turned html files into hds

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const hbs = require("hbs");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(express.static(__dirname)); //Set the default directory path to "book-store-main"
+
+app.set("view engine", "hbs"); //Install hbs
+
+app.get("/", function (_, response) { //Making it so localhost:3000 is turned into localhost:3000/home
+    response.sendFile(__dirname + "/index.hbs");
+});
+
+app.use("/home", function(request, response){ //show hbs files
+    response.render("index.hbs");
+});
+
+app.use("/genres", function(request, response){
+    response.render("genres.hbs");
+});
 
 app.get('/', (req, res) => {
-    console.log('Listening!');
-  });
+	console.log('Listening!');
+});
 
-  app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`);
-  });
+app.listen(port, () => {
+	console.log(`Server is listening on localhost:${port}`);
+});
+
