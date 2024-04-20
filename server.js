8cca18e0c3c5412e1334901e437ce913a9454deb
Justnme <tpopokghfjpoik@gmail.com>
2024-04-20 14:12:15 +0300

    #1 minor fixis

--- server.js
+++ server.js
@@ -12,241 +12,237 @@ const port = 3000;
 // #4 hbs conf
 const path = require('path');
 
-app.set("view engine", "hbs"); 
-app.set("views", __dirname + "/views"); 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-// ---------DATABASE------------->>>
+// Register partials directory
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize({
-	dialect: "sqlite",
-	storage: "bookStore.db",
-	define: {
-		timestamps: false
-	}
+app.use(express.static(__dirname));
+
+app.get('/', (_, response) => {
+    response.render('index');
 });
 
-const Users = sequelize.define("Users", {
-  user_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  password_text: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  login: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  status_text: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  address: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
+app.get('/home', (_, response) => {
+    response.render('index');
 });
 
-const Authors = sequelize.define("Authors", {
-  author_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  full_name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
+app.get('/genres', (_, response) => {
+    response.render('genres');
 });
 
-const Books = sequelize.define("Books", {
-  book_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  genre_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  image_name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  author_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
 
-const Applications = sequelize.define("Applications", {
-  application_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  status_text: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  book_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
+// // ---------DATABASE------------->>>
 
-const Sales = sequelize.define("Sales", {
-  sale_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  application_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  book_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize({
+// 	dialect: "sqlite",
+// 	storage: "bookStore.db",
+// 	define: {
+// 		timestamps: false
+// 	}
+// });
 
-const BookTags = sequelize.define("BookTags", {
-  book_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  tag_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  }
-});
+// const Users = sequelize.define("Users", {
+//   user_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   password_text: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   login: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   status_text: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   address: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
 
-const Tags = sequelize.define("Tags", {
-  tag_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  tag_name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
+// const Authors = sequelize.define("Authors", {
+//   author_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   full_name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
 
-const Genres = sequelize.define("Genres", {
-  genre_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  genre_name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
+// const Books = sequelize.define("Books", {
+//   book_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   genre_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   image_name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   author_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false,
+//   },
+//   date: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
 
-const Reviews = sequelize.define("Reviews", {
-  review_id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  user_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  book_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  rating: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-  content: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  date: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  }
-});
+// const Applications = sequelize.define("Applications", {
+//   application_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   user_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   status_text: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   book_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   date: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
 
-sequelize.sync().then(result=>{
-  console.log(result);
-})
-.catch(err=> console.log(err));
+// const Sales = sequelize.define("Sales", {
+//   sale_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   user_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   application_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   book_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   date: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
+
+// const BookTags = sequelize.define("BookTags", {
+//   book_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   tag_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   }
+// });
+
+// const Tags = sequelize.define("Tags", {
+//   tag_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   tag_name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
+
+// const Genres = sequelize.define("Genres", {
+//   genre_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   genre_name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
+
+// const Reviews = sequelize.define("Reviews", {
+//   review_id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     allowNull: false,
+//   },
+//   user_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   book_id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   rating: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//   },
+//   content: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   date: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   }
+// });
+
+// sequelize.sync().then(result=>{
+//   console.log(result);
+// })
+// .catch(err=> console.log(err));
 
 // <<<---------DATABASE-------------
 
 
-// #4 hbs conf
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
-app.set("view engine", "hbs"); // Install hbs
-app.set("views", __dirname + "/views"); // Set views directory
-
-app.use(express.static(__dirname)); 
-app.use(express.static(__dirname)); // Set the default directory path to serve static files
-
-app.get("/", (_, response) => {
-    response.render("index"); 
-    response.render("index"); // Render views/index.hbs
-});
-
-app.get("/home", (_, response) => {
-    response.render("index");
-    response.render("index"); // Render views/index.hbs
-});
-
-app.get("/genres", (_, response) => {
-    response.render("genres"); 
-    response.render("genres"); // Render views/genres.hbs
-});
 
 app.listen(port, () => {
     console.log(`Server is listening on localhost:${port}`);
