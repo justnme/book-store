58def9d790d57dac3a41c082b18197e0d6d4542c
Justnme <tpopokghfjpoik@gmail.com>
2024-04-28 19:59:35 +0300

    Cart is now glabl

--- scripts/shopping-cart.js
+++ scripts/shopping-cart.js
@@ -39,24 +39,6 @@ function getCookie(name) {
 }
 
     
-
-
-function changeQuantityCart() {
-    let cookies = document.cookie.split(';');
-    console.log("All cookies:", cookies);
-    let cartCookies = cookies.filter(cookie => cookie.trim().startsWith('cart_'));
-    console.log("Cart cookies:", cartCookies);
-    let quantity = cartCookies.length;
-  
-    let shoppingIdElement = document.getElementById('shoppingId');
-    if (shoppingIdElement) {
-        shoppingIdElement.textContent = quantity.toString();
-    }
-  
-    console.log(`Number of items in cart was updated to ${quantity}`);
-}
-
-
 function cookiesBooksToJs() {
     let cookies = document.cookie.split(';');
     let cartItems = [];
